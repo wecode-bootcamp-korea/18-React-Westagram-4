8ada8f47bf962b68785c00e2b2d6b7defdda47b2
hyjoong <maplesosser@naver.com>
2021-03-06 15:52:20 +0900

    메인화면 & 로그인 화면 버튼 활성화 mission 2

--- src/Pages/hyunjoongkim/Main/Main.js
+++ src/Pages/hyunjoongkim/Main/Main.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MainKim extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
 export default MainKim;
