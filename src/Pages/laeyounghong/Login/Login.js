74b6bbbacd0e9d4f1d3f39be8e9ffa7122041057
18기 홍래영 <honglang0328@gmail.com>
2021-03-08 13:47:51 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/Pages/laeyounghong/Login/Login.js
+++ src/Pages/laeyounghong/Login/Login.js
@@ -1,13 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Login.scss";
+import "../Styles/reset.scss";
+import "../Styles/common.scss";
 
 class LoginHong extends Component {
-    render() {
-        return (
-            <div>
-                
+  constructor() {
+    super();
+    this.state = {
+      Id: "",
+      Password: "",
+    };
+  }
+
+  handleLoginInput = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  loginValidation = () => {
+    return this.state.Id.includes("@") && this.state.Password.length >= 5;
+  };
+
+  render() {
+    return (
+      <div className="LoginHong">
+        <section className="wrapper">
+          <h1 className="logo">Westagram</h1>
+          <form className="loginBox">
+            <div className="textBox">
+              <input
+                name="Id"
+                type="text"
+                onChange={this.handleLoginInput}
+                placeholder="전화번호, 사용자 이름 또는 이메일"
+              />
             </div>
-        );
-    }
+            <div className="textBox">
+              <input
+                name="Password"
+                type="password"
+                onChange={this.handleLoginInput}
+                placeholder="비밀번호"
+              />
+            </div>
+            <Link to={this.loginValidation() ? "/main-hong" : "/login-hong"}>
+              <button
+                className={
+                  this.loginValidation() ? "loginBtn" : "loginBtn disabled"
+                }
+              >
+                로그인
+              </button>
+            </Link>
+          </form>
+          <span>
+            <a href="https://www.google.co.kr">비밀번호를 잊으셨나요?</a>
+          </span>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default LoginHong;
